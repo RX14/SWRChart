afdcd0eac586e0b918ac4db946c251c93e16d511
RX14 <chris@rx14.co.uk>
2013-12-15 21:57:15 +0000

    Populate ComPortSelection with COM Ports
    + Show SettingsDialog when button clicked

--- src/MainForm.vb
+++ src/MainForm.vb
@@ -3,6 +3,12 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         DrawPoint(15, 4)
         DrawPoint(10, 9)
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            SettingsDialog.ComPortSelection.Items.Add(sp)
+        Next
     End Sub
 
+    Private Sub SettingsButton_Click(sender As Object, e As EventArgs) Handles SettingsButton.Click
+        SettingsDialog.Show()
+    End Sub
 End Class
