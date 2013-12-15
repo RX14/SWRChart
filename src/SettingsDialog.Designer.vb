149ed88c75085c6a045a5e31d7f0693750aae886
RX14 <chris@rx14.co.uk>
2013-12-15 21:57:56 +0000

    Rename ComboBox1 to ComPortSelection + add label

--- src/SettingsDialog.Designer.vb
+++ src/SettingsDialog.Designer.vb
@@ -22,44 +22,44 @@ Partial Class SettingsDialog
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComPortSelection = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'ComboBox1
+        'ComPortSelection
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(108, 79)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
-        Me.ComboBox1.TabIndex = 1
+        Me.ComPortSelection.FormattingEnabled = True
+        Me.ComPortSelection.Location = New System.Drawing.Point(68, 12)
+        Me.ComPortSelection.Name = "ComPortSelection"
+        Me.ComPortSelection.Size = New System.Drawing.Size(121, 21)
+        Me.ComPortSelection.TabIndex = 1
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(50, 13)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Com Port"
         '
         'SettingsDialog
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 259)
+        Me.ClientSize = New System.Drawing.Size(201, 44)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ComPortSelection)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "SettingsDialog"
         Me.ShowIcon = False
-        Me.Text = "Form1"
+        Me.Text = "Settings"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComPortSelection As System.Windows.Forms.ComboBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
