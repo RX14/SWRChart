abaa0d5476c5c5287775de24ec3880135f3abf7f
RX14 <chris@rx14.co.uk>
2013-12-15 22:29:32 +0000

    Replace Test code to better reflect real useage!

--- src/MainForm.vb
+++ src/MainForm.vb
@@ -11,7 +11,10 @@
     End Sub
 
     Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
-        DrawPoint(15, 4)
-        DrawPoint(10, 9)
+        DrawPoint(150000000, 2.0)
+        DrawPoint(155000000, 1.5)
+        DrawPoint(160000000, 1.1)
+        DrawPoint(165000000, 1.3)
+
     End Sub
 End Class
