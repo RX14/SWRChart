35a7aade60ecb138769db485e53f3bd02d6c0820
RX14 <chris@rx14.co.uk>
2013-12-15 19:59:49 +0000

    Add User Code

---
+++ src/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.Chart = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.StartButton = New System.Windows.Forms.Button()
+        CType(Me.Chart, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Chart
+        '
+        Me.Chart.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        ChartArea1.AxisX.Title = "Frequency"
+        ChartArea1.AxisY.Title = "SWR"
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart.ChartAreas.Add(ChartArea1)
+        Me.Chart.Location = New System.Drawing.Point(0, 0)
+        Me.Chart.Name = "Chart"
+        Series1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight
+        Series1.ChartArea = "ChartArea1"
+        Series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline
+        Series1.Name = "Series1"
+        Me.Chart.Series.Add(Series1)
+        Me.Chart.Size = New System.Drawing.Size(300, 264)
+        Me.Chart.TabIndex = 0
+        Me.Chart.Text = "Chart1"
+        '
+        'StartButton
+        '
+        Me.StartButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.StartButton.Location = New System.Drawing.Point(7, 270)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(75, 23)
+        Me.StartButton.TabIndex = 1
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(300, 300)
+        Me.Controls.Add(Me.StartButton)
+        Me.Controls.Add(Me.Chart)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Chart, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Chart As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents StartButton As System.Windows.Forms.Button
+
+End Class
---
+++ src/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Chart.Series.Item(0).Points.AddXY(12, 41)
+    End Sub
+
+    Private Sub DrawPoint(X As Integer, Y As Integer)
+        Chart.Series.Item(0).Points.AddXY(X, Y)
+    End Sub
+
+End Class
