dd05eb2f2aea221c589793db84e9910b1447de6d
RX14 <chris@rx14.co.uk>
2013-12-15 22:23:35 +0000

    Moved test Code to Button Click Event

--- src/MainForm.vb
+++ src/MainForm.vb
@@ -1,8 +1,6 @@
 ﻿Public Class MainForm
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        DrawPoint(15, 4)
-        DrawPoint(10, 9)
         For Each sp As String In My.Computer.Ports.SerialPortNames
             SettingsDialog.ComPortSelection.Items.Add(sp)
         Next
@@ -10,5 +8,10 @@
 
     Private Sub SettingsButton_Click(sender As Object, e As EventArgs) Handles SettingsButton.Click
         SettingsDialog.Show()
+    End Sub
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        DrawPoint(15, 4)
+        DrawPoint(10, 9)
     End Sub
 End Class
