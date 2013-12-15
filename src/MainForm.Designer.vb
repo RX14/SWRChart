7993364750819c61aede10590042c9a44b7402c5
RX14 <chris@rx14.co.uk>
2013-12-15 21:33:37 +0000

    Add Settings button with gear icon

--- src/MainForm.Designer.vb
+++ src/MainForm.Designer.vb
@@ -22,11 +22,13 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim ChartArea3 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
-        Dim Series3 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
         Me.Chart = New System.Windows.Forms.DataVisualization.Charting.Chart()
         Me.StartButton = New System.Windows.Forms.Button()
         Me.StatusLabel = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.Chart, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -35,17 +37,17 @@ Partial Class MainForm
         Me.Chart.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        ChartArea3.AxisX.Title = "Frequency"
-        ChartArea3.AxisY.Title = "SWR"
-        ChartArea3.Name = "ChartArea1"
-        Me.Chart.ChartAreas.Add(ChartArea3)
+        ChartArea1.AxisX.Title = "Frequency"
+        ChartArea1.AxisY.Title = "SWR"
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart.ChartAreas.Add(ChartArea1)
         Me.Chart.Location = New System.Drawing.Point(0, 0)
         Me.Chart.Name = "Chart"
-        Series3.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight
-        Series3.ChartArea = "ChartArea1"
-        Series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline
-        Series3.Name = "Series1"
-        Me.Chart.Series.Add(Series3)
+        Series1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight
+        Series1.ChartArea = "ChartArea1"
+        Series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline
+        Series1.Name = "Series1"
+        Me.Chart.Series.Add(Series1)
         Me.Chart.Size = New System.Drawing.Size(300, 264)
         Me.Chart.TabIndex = 0
         Me.Chart.Text = "Chart1"
@@ -70,11 +72,25 @@ Partial Class MainForm
         Me.StatusLabel.TabIndex = 2
         Me.StatusLabel.Text = "Idle"
         '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.SystemColors.Control
+        Me.Button1.Location = New System.Drawing.Point(267, 269)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(27, 27)
+        Me.Button1.TabIndex = 3
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(300, 300)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.StatusLabel)
         Me.Controls.Add(Me.StartButton)
         Me.Controls.Add(Me.Chart)
@@ -89,5 +105,6 @@ Partial Class MainForm
     Friend WithEvents Chart As System.Windows.Forms.DataVisualization.Charting.Chart
     Friend WithEvents StartButton As System.Windows.Forms.Button
     Friend WithEvents StatusLabel As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
