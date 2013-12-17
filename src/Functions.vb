a3a38356ee217e01aa9f9b1e6be23cb7d724ab9b
RX14 <chris@rx14.co.uk>
2013-12-17 19:09:55 +0000

    Minor Changes
    + Make RecieveSerialData a Public function
    + Rename SerialPort to com because i caynn

--- src/Functions.vb
+++ src/Functions.vb
@@ -4,16 +4,16 @@
     End Sub
 
     'This code was taken from a Microsoft Example Code Snippet.
-    Function ReceiveSerialData(ComPort As String) As String
+    Public Function ReceiveSerialData(ComPort As String) As String
         ' Receive strings from a serial port. 
         Dim returnStr As String = ""
 
-        Dim com1 As IO.Ports.SerialPort = Nothing
+        Dim com As IO.Ports.SerialPort = Nothing
         Try
-            com1 = My.Computer.Ports.OpenSerialPort(ComPort)
-            com1.ReadTimeout = 10000
+            com = My.Computer.Ports.OpenSerialPort(ComPort)
+            com.ReadTimeout = 10000
             Do
-                Dim Incoming As String = com1.ReadLine()
+                Dim Incoming As String = com.ReadLine()
                 If Incoming Is Nothing Then
                     Exit Do
                 Else
@@ -23,7 +23,7 @@
         Catch ex As TimeoutException
             returnStr = "Error: Serial Port read timed out."
         Finally
-            If com1 IsNot Nothing Then com1.Close()
+            If com IsNot Nothing Then com.Close()
         End Try
 
         Return returnStr
