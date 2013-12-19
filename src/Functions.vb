15c76b62b0d6e3f2349a617736210bd18de6e929
RX14 <chris@rx14.co.uk>
2013-12-19 21:28:06 +0000

    Fails + Adding Comments
    + Comments
    + Acctually use the right range of ASCII... (numbers are one out because
    we want to incl. 48 -57)
    + Pass only 1st char to Asc()
    + (Hopefully) Use stdOut now...
    + Added an actual use of the Status Label
    + Change StartButton text to stop when com opened.

--- src/Functions.vb
+++ src/Functions.vb
@@ -1,34 +1,43 @@
 ﻿Module Functions
     Public WithEvents com As New IO.Ports.SerialPort
     Public Sub DrawPoint(X As Double, Y As Double, Optional SeriesRef As Integer = 0)
+        'Draws a point on the graph given X + Y coordinates
         MainForm.Chart.Series.Item(SeriesRef).Points.AddXY(X, Y)
     End Sub
 
     Public Sub OpenPort(ComPort As String)
+        'Opens port when given the name of a COM port
         Try
+            'Set COM port setttings
             com.PortName = ComPort
             com.BaudRate = 9600
             com.Parity = IO.Ports.Parity.None
             com.DataBits = 8
             com.StopBits = IO.Ports.StopBits.One
+            'Open comport
             com.Open()
+            MainForm.StatusLabel.Text = "ComPort Opened... Reading Data"
         Catch ex As Exception
+            'Open Messagebox on failure
             MsgBox(ex.ToString)
         End Try
 
     End Sub
 
     Private Sub com_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles com.DataReceived
-        Dim line As String = com.ReadLine
-        If Asc(line) < 90 And Asc(line) > 48 Then
-            'Console.WriteLine("DEBUG: COMDATA: " + line)
+        'Handles recieving COM data
+        Dim line As String = com.ReadLine 'For storing the read line
+        'If the first character is a number then...
+        If Asc(line(0)) < 58 And Asc(line(0)) > 47 Then
+            'Console.Out.WriteLine("DEBUG: COMDATA: " + line)
             Dim lineSplit As String() = line.Split(",")
             Dim X As Double = Convert.ToInt32(lineSplit(0))
             Dim Y As Double = Convert.ToInt32(lineSplit(1))
-            Console.WriteLine("DEBUG: COMPARSEDDATA: " + X + ";" + Y)
+            Console.Out.WriteLine("DEBUG: COMPARSEDDATA: " + X + ";" + Y)
             DrawPoint(X, Y)
         Else
-            Console.WriteLine("DEBUG: COMDEBUG: " + line)
+            'Write other lines to console as debug datas
+            Console.Out.WriteLine("DEBUG: COMDEBUG: " + line)
         End If
     End Sub
 End Module
